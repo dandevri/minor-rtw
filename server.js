8fca11792273aa3778195f97b22cfaa2ccbc517f
Danny de Vries <vriesdan@gmail.com>
2017-04-10 15:37:51 +0200

    :sparkles: add templating engine ejs

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static('src'));
 app.set('view engine', 'ejs');
-app.set('views', '/views');
+app.set('views', 'views');
 
 app.get('/', function (req, res) {
   res.render('index');
