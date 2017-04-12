257fe65d97fca8abcdb827f6a96ca7ebd09c801f
Danny de Vries <vriesdan@gmail.com>
2017-04-12 11:14:29 +0200

    :bulb: add code comments for express

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+// Load libraries and modules
 var express = require('express');
 
 var app = express();
 
+// GET, POST etc. go through static folder to look for files
 app.use(express.static('src'));
+
+// Set templating engine to ejs
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+// Middleware to render page
 app.get('/', function (req, res) {
   res.render('index');
 });
 
 app.listen(3000, function () {
-  console.log('App is listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
