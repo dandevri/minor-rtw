d21590041fc472c6bcbfc3089e08940de4f206e9
Danny de Vries <vriesdan@gmail.com>
2017-04-19 14:56:13 +0200

    :bulb: Add code comments to server file

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ var express = require('express');
 
 var app = express();
 
+// Add environment variables for deployment
 var port = process.env.PORT || '3000';
 var host = process.env.HOST || '0.0.0.0';
 
+// Set src as static folder
 app.use(express.static('src'));
+
+// Set view engine to ejs and views folder
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+// Render index.ejs if you get response
 app.get('/', function (req, res) {
   res.render('index.ejs');
 });
 
-app.listen(3000, function () {
+app.listen(port, host, function () {
   console.log('It has works on', host, port);
 });
