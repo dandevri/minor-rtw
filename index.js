6ddb9875a5914c7420571427e244fcb935c63f83
Pers_ <danny@161.199.028.145.hva.nl>
2017-05-01 16:31:34 +0200

    :wrench: Add playlistItem request

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var dot = require('dotenv').config();
 var app = express();
 
 // Add environment variables for deployment
-var port = process.env.PORT || 3100;
+var port = process.env.PORT || 3000;
 var host = process.env.HOST || '0.0.0.0';
 
 // Set src as static folder
@@ -38,20 +38,41 @@ app.get('/oauth', function (req, res) {
         grant_type: 'authorization_code'
       }
     }, function (err, httpResponse, body) {
-      console.log(body);
+      // console.log(body);
+
       // Parse the response to json. We have access to the token here.
       var accessToken = JSON.parse(body);
 
+      // Make request to Google Api to get list of playlists.
       request('https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true&access_token=' + accessToken.access_token,
         function (err, httpResponse, body) {
-          console.log(body);
-          var data = JSON.parse(body);
-          res.render('pages/list.ejs', {data: data});
+          // console.log(body);
+          var playlists = JSON.parse(body);
+
+          // Loop over all the items in de playlist
+          var urls = playlists.items.map(function (item) {
+            return 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=' + item.id + '&access_token=' + accessToken.access_token;
+          });
+
+          var playlistInfo = [];
+          var completedRequests = 0;
+
+          urls.forEach(function (url) {
+            request(url, function (err, httpResponse, body) {
+              // Push the playlist info to the array.
+              playlistInfo.push(JSON.parse(body));
+              completedRequests++;
+              if (completedRequests === urls.length) {
+                console.log('im done. :tada:');
+                console.log(playlistInfo);
+                res.render('pages/list.ejs', {playlists: playlists, playlistInfo: playlistInfo});
+              }
+            });
+          });
         }
       );
     });
 });
-
 // Starts a UNIX socket and listen for connections
 app.listen(port, host, function () {
   console.log('It has works on', host, port);
