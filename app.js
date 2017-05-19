96a1fa5f262034e51ee98dea53ca7c1f9b85a5f4
dandevri <vriesdan@gmail.com>
2017-05-19 11:59:31 +0200

    :sparkles: Add index page

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ var app = express();
 var port = process.env.PORT || '3000';
 var host = process.env.HOST || '0.0.0.0';
 
+app.use(express.static('src'))
+  .set('views', 'views')
+  .set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.send('Hello world!');
+  res.render('index');
 });
 
 app.listen(port, function () {
