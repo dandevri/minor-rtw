2eb51a476926b747c6bc70725d8f0baaac8a44f8
dandevri <vriesdan@gmail.com>
2017-05-19 17:02:23 +0200

    :heavy_plus_sign: Add socket dependency

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+// Import Express module - have acces to
 var express = require('express');
 
+// Trigger express function
 var app = express();
 
 var port = process.env.PORT || '3000';
 var host = process.env.HOST || '0.0.0.0';
 
+// Host the files (make public client side)
 app.use(express.static('src'))
   .set('views', 'views')
   .set('view engine', 'ejs');
