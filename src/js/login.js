7e3a9d60e4a63f2dd2ae131612d54f00064f1bff
dandevri <vriesdan@gmail.com>
2017-05-19 15:46:39 +0200

    :white_check_mark: Playing around with welcomescreen

--- src/js/login.js
+++ src/js/login.js
@@ -1,9 +1,23 @@
+// Store for later use
+var profileName = '';
+var profileImage = '';
+var profileEmail = '';
+
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  profileName = profile.getName();
+  console.log(profileName);
+  profileImage = profile.getImageUrl();
+  console.log(profileImage);
+  profileEmail = profile.getEmail();
+  console.log(profileEmail);
 };
+
+function welcomeScreen() {
+  document.querySelector('.welcome').innerHTML +=
+  `<p>Hi ` + profileName + `</p>` +
+  `<img src="` + profileImage + `">`;
+}
 
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
