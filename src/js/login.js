87efb2873de5404967a992f20ce522f77f87d14c
dandevri <vriesdan@gmail.com>
2017-05-20 16:08:57 +0200

    :hammer: Fix login screen

--- src/js/login.js
+++ src/js/login.js
@@ -11,12 +11,17 @@ function onSignIn(googleUser) {
   console.log(profileImage);
   profileEmail = profile.getEmail();
   console.log(profileEmail);
-};
+  addProfileScreen();
+  removeSignButton();
+}
 
-function welcomeScreen() {
-  document.querySelector('.welcome').innerHTML +=
-  `<p>Hi ` + profileName + `</p>` +
-  `<img src="` + profileImage + `">`;
+function addProfileScreen() {
+  document.querySelector('.profile').innerHTML +=
+  `<div class="user">` +
+    `<p>Hi ` + profileName + `</p>` +
+    `<img src="` + profileImage + `">` +
+    `<a href="/room">Go to room</a>` +
+  `</div>`;
 }
 
 function signOut() {
@@ -24,4 +29,21 @@ function signOut() {
   auth2.signOut().then(function () {
     console.log('User signed out.');
   });
+  removeProfileScreen();
+}
+
+function removeSignButton() {
+  var selectButton = document.querySelector('.login').classList.add('hidden');
+}
+
+function addSignButton() {
+  var selectButton = document.querySelector('.login').classList.remove('hidden');
+}
+
+function removeProfileScreen() {
+  // Remove the user div
+  var selectProfile = document.querySelector('.profile');
+  var selectUser = document.querySelector('.user');
+  selectProfile.removeChild(selectUser);
+  addSignButton();
 }
